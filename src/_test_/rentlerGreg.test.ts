8bd19776a151d0cd3bf180a2797e9d3df25c8f18
GregoryDalessandro <gdalessandro418@gmail.com>
2021-03-14 09:24:01 -0400

    commented out sign up/sign in tests, added tests for buttons, hyperlinks, filters persistence

--- src/_test_/rentlerGreg.test.ts
+++ src/_test_/rentlerGreg.test.ts
@@ -1,5 +1,4 @@
 import {RentlerPage} from "../signInUpPage"
-const chromedriver = require("chromedriver");
 import {
     By,
     Builder,
@@ -10,26 +9,45 @@ import {
     Browser
 } from "selenium-webdriver";
 
+const chromedriver = require("chromedriver");
 const driver: WebDriver = new Builder()
   .withCapabilities(Capabilities.chrome())
   .build();
   const rentler = new RentlerPage(driver);
 
-  test("Cannot sign in with invalid email or password", async () => {
-      await rentler.navigate();
-      await rentler.signingIn();
-      await rentler.enteringEmail("natalie.halloran@yahoo.com");
-      await rentler.enteringPassword("12345678");
-      expect(await rentler.invalidSignin()).toContain("Invalid username or password");
-  });
-   test("Can sign up with valid email address", async () => {
+  describe("rentler", () => {
+    beforeEach(async () => {
         await rentler.navigate();
-        await rentler.signingUp();
-        await rentler.enteringName("Natalie","Halloran");
-        await rentler.enteringEmail("tohog61616@naymio.com");
-        await rentler.enteringPassword("12345678");
-        await rentler.retypingPassword("12345678");
-        await rentler.landlordOrTenant();
-        await rentler.agreeToTermsSignUp();
+      });
+    afterEach(async () => {
+        await rentler.driver.quit();
     });
+    // test("Cannot sign in with invalid email or password", async () => {
+    //     await rentler.signingIn();
+    //     await rentler.enteringEmail("natalie.halloran@yahoo.com");
+    //     await rentler.enteringPassword("12345678");
+    //     expect(await rentler.invalidSignin()).toContain("Invalid username or password");
+    // });
+    //  test("Can sign up with valid email address", async () => {
+    //       await rentler.signingUp();
+    //       await rentler.enteringName("Natalie","Halloran");
+    //       await rentler.enteringEmail("tohog61616@naymio.com");
+    //       await rentler.enteringPassword("12345678");
+    //       await rentler.retypingPassword("12345678");
+    //       await rentler.landlordOrTenant();
+    //       await rentler.agreeToTermsSignUp();
+    //   });
+      test("Can use search button on the main page", async () => {
+
+      });
+      test("Can use hyperlinks to navigate to other endpoints", async() => {
+
+      });
+      test("Verify if filters persist", async() => {
+
+      });
+  });
+
+
+
 
