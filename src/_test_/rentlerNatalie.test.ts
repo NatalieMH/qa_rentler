09d4da53a3c998a8f650f6c041b82f1aa12424e3
Natalie Halloran <natalie.halloran@yahoo.com>
2021-03-18 18:26:44 -0400

    Added comments

--- src/_test_/rentlerNatalie.test.ts
+++ src/_test_/rentlerNatalie.test.ts
@@ -18,7 +18,7 @@ const driver: WebDriver = new Builder()
     test("Cannot sign in with invalid email and password", async () => {
       await rentler.navigate();
       await rentler.signingIn();
-      await rentler.enteringEmail("natalie.halloran@yahoo.com");
+      await rentler.enteringEmail("natalie.halloran@yahoo.com"); //natalie.halloran@yahoo.com has not been signed up
       await rentler.enteringPassword("12345678\n");
       expect(await rentler.invalidSignin()).toContain("Invalid username or password");
       });
@@ -26,14 +26,14 @@ const driver: WebDriver = new Builder()
     test("Cannot sign in with valid email but invalid password", async () => {
         await rentler.navigate();
         await rentler.signingIn();
-        await rentler.enteringEmail("marsqaemail@gmail.com");
-        await rentler.enteringPassword("1234\n");
-        expect(await rentler.passwordShort()).toBeTruthy();
+        await rentler.enteringEmail("marsqaemail@gmail.com"); //marsqaemail@gmail.com has been signed up
+        await rentler.enteringPassword("1234\n"); //This is not the password for marsquaemail@gmail.com, it is also too short.
+        expect(await rentler.passwordShort()).toBeTruthy(); //The data element that includes "Password must be between 7 and 100 characters" has been returned
     });
    test("Can sign up with valid email address", async () => { //SignUp cannot be done by automation, but the fields can be entered automatically
         await rentler.navigate();
         await rentler.signingUp();
-        await rentler.enteringName("Natalie","Halloran");
+        await rentler.enteringName("Natalie","Halloran"); //The enteringName function takes 2 arguments, so both the first and last name can be entered through one method.
         await rentler.enteringEmail("tohog61616@naymio.com");
         await rentler.enteringPassword("12345678");
         await rentler.retypingPassword("12345678");
@@ -43,15 +43,15 @@ const driver: WebDriver = new Builder()
     test("Can sign in using valid email address", async () => {
         await rentler.navigate();
         await rentler.signingIn();
-        await rentler.enteringEmail("marsqaemail@gmail.com");
-        await rentler.enteringPassword("1234567\n");
+        await rentler.enteringEmail("marsqaemail@gmail.com");//This email address was used to sign up manually
+        await rentler.enteringPassword("1234567\n"); //This is the correct password for marsqaemail@gmail.com
         await rentler.useAsTenant();
-        expect(await rentler.welcomePage()).toContain("Welcome");
+        expect(await rentler.welcomePage()).toContain("Welcome"); //Once "Use As Tenant" has been chosen the user will be brought to the Welcome page
     });
     test("Can favorite a listing", async () => {
         await rentler.navigate();
         await rentler.signingIn();
-        await rentler.favoriteListing();
-        expect(await rentler.showMyPlaces()).toContain("1 Result");
+        await rentler.favoriteListing(); //A listing should be favorited, but this only works sometimes due to a bug with the map
+        expect(await rentler.showMyPlaces()).toContain("1 Result");//Once a listing is favorited, My Place is clicked and the one favorited listing appears under My Properties
     });
    
--- src/signInUpPage.ts
+++ src/signInUpPage.ts
@@ -9,17 +9,6 @@ import {
 } from "selenium-webdriver";
 
 export class RentlerPage extends BasePage {
-    //driver: WebDriver;
-    //url: string = 'https://rentler.com';
-    //signIn: By = By.css('.nav-item-sign-in');
-   // signUp: By = By.css('.nav-bar')
-    //searchCity: By = By.id('Location');
-   // searchInProperties: By = By.css('input[aria-label = "Search"]');
-   // minPrice: By = By.id('minprice');
-    //maxPrice: By = By.id('maxprice');
-    //bedBath: By = By.css('.bed-bath');
-    //rentalPrice: By = By.css('.price');
-    //rentalAddress: By = By.css('.address');
     firstName: By = By.id('Input_FirstName');
     lastName: By = By.id('Input_LastName');
     emailField: By = By.css('input[type = "email"]');
@@ -31,6 +20,13 @@ export class RentlerPage extends BasePage {
     invalidLogin: By = By.css('.alert-danger');
     termsConditions: By = By.css('label[for = "Input_AgreeToTerms"]');
     signUpButton: By = By.css('button[type = "submit"]');
+    tenantOrLandlord: By = By.xpath('(//div[@class = "choice-figure"])[1]');
+    welcome: By = By.css('nav[aria-label = "breadcrumb');
+    favorites: By = By.xpath('(//*[@class = "btn-favorite"])[1]');
+    passwordLength: By = By.css('input[data-val-length="Password must be between 7 and 100 characters"]');
+    sListSigned: By = By.xpath('//li/a[contains(text(),"Search Listings")]');
+    myPlaces: By = By.css('.link-favorite');
+    resultList: By = By.css('.zeta.search-result-text');
     constructor(driver:WebDriver) {
         super(driver);
     }
@@ -48,8 +44,7 @@ export class RentlerPage extends BasePage {
     }
     async enteringPassword(password: string) {
         await click(this.driver,this.passwordField);
-        await this.driver.switchTo().activeElement().sendKeys(`${password}\n`);
-        await this.driver.wait(until.elementLocated(this.passwordField));
+        await this.driver.switchTo().activeElement().sendKeys(`${password}`);
     }
     async retypingPassword(password2: string) {
         await click(this.driver,this.retypePassword);
@@ -70,15 +65,42 @@ export class RentlerPage extends BasePage {
         console.log(errorMessage);
         return errorMessage;
     }
+    async passwordShort() {
+        await click(this.driver,this.passwordField);
+        await this.driver.wait(until.elementLocated(this.passwordLength));
+        return this.passwordLength;
+    }
     async landlordOrTenant() {
         await click(this.driver,this.selectType);
         await this.driver.wait(until.elementLocated(this.landlord));
         await click(this.driver,this.landlord);
-        //await click(this.driver,this.landlord);
     }
     async agreeToTermsSignUp() {
         await click(this.driver,this.termsConditions);
         await click(this.driver,this.signUpButton);
+    }
+    async useAsTenant() {
+        await this.driver.wait(until.elementLocated(this.tenantOrLandlord));
+        await this.driver.findElement(this.tenantOrLandlord);
+        await click(this.driver,this.tenantOrLandlord);
+        await this.driver.wait(until.elementLocated(this.welcome));
+    }
+    async welcomePage() {
+        await this.driver.wait(until.elementLocated(this.welcome));
+        let welcomeScreen = await (await (this.driver.findElement(this.welcome))).getText();
+        return welcomeScreen;
+    }
+    async favoriteListing() {
+        await click(this.driver,this.sListSigned);
+        await this.driver.wait(until.elementLocated(this.favorites));
+        await click(this.driver,this.favorites);
+        await this.driver.wait(until.elementLocated(By.css('div[data-liked = "true"]')));
+    }
+    async showMyPlaces() {
+        await click(this.driver,this.myPlaces);
+        await this.driver.wait(until.elementLocated(this.resultList));
+        let results = await (await (this.driver.findElement(this.resultList))).getText();
+        return results;
     }
 }
     const click = async function (driver, elementBy: By) {
